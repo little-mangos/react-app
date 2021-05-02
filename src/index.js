530b8130ec74c149a4e3ee8d0d441837dd4da9fa
little-mangos <zss_wh@163.com>
2021-05-02 13:14:09 +0800

    github

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <h1>hello github</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
